67f0b993c5a01d8fb3c5c4a575eaddbf47d11080
Shubham Choudhary <shubham.22b0121008@abes.ac.in>
2026-03-30 10:06:43 +0530

    updated some line in MyController.java

--- 28-03-2026/Movie App Assignment/src/main/java/com/cg/controllers/MyController.java
+++ 28-03-2026/Movie App Assignment/src/main/java/com/cg/controllers/MyController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.cg.entities.Movie;
 import com.cg.service.IMovieService;
 
