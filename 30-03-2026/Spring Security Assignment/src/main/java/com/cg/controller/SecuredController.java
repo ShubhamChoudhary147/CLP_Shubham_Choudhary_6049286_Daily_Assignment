2739b72984a27104d98f27ee0fca0be8beca502f
Shubham Choudhary <shubham.22b0121008@abes.ac.in>
2026-04-01 12:28:40 +0530

    Spring Security Assignment is uploaded

---
+++ 30-03-2026/Spring Security Assignment/src/main/java/com/cg/controller/SecuredController.java
@@ -0,0 +1,24 @@
+package com.cg.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecuredController {
+	@GetMapping("/publicEndPoint")
+	public String forPublic() {
+		return "This is a public endpoint accessible to everyone.";
+	}
+	
+	@GetMapping("/SecuredUserEndPoint")
+	@PreAuthorize("hasAnyRole('USER','ADMIN')")
+	public String forUser() {
+		return "This is a secured endpoint accessible to users with USER role only.";
+	}
+	@GetMapping("/SecuredAdminEndPoint")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String forAdmin() {
+		return "This is a secured endpoint accessible to users with ADMIN role only.";
+	}
+}
